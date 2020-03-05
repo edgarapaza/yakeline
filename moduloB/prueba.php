9f2451b6c1da91e5ac010d42d853912ce7df4170
DThalia <daniithaliarp@gmial.com>
2020-03-05 13:25:40 -0500

    prototipo de catalogo

---
+++ moduloB/prueba.php
@@ -0,0 +1,50 @@
+<link rel="stylesheet" type="text/css" href="style.css">
+<div class="contain">
+
+  <div class="wrapper">
+    <div class="contacts">
+      <h3>Our contacts</h3>
+
+      <ul>
+        <li>San Joe St.</li>
+        <li>00-1212121-11</li>
+        <li>mail@mail.com</li>
+      </ul>
+    </div>
+
+    <div class="form">
+      <h3>Send us a message</h3>
+      <form action="">
+        <p>
+          <label for="">Your name</label>
+          <input type="text">
+        </p>
+        <p>
+          <label for="">Skype</label>
+          <input type="text">
+        </p>
+        <p>
+          <label for="">Email Address</label>
+          <input type="text">
+        </p>
+        <p>
+          <label for="">Topic</label>
+          <input type="text">
+        </p>
+        <p>
+          <label for="">Write your message</label>
+          <textarea name="" id="" cols="30" rows="7"></textarea>
+        </p>
+        <p>
+          <button>Send</button>
+        </p>
+      </form>
+    </div>
+  </div>
+</div>
+dfwsrfe
+<input type="text" name="texto1" placeholder="Acceso con a" accesskey="a">
+sfsdf
+<input type="text" name="texto2" placeholder="Acceso con i" accesskey="i">
+<input type="text" name="texto3" placeholder="Acceso con o" accesskey="o">
+<input type="text" name="texto4" placeholder="Acceso con u" accesskey="u">
