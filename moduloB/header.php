0a91a9a48846d0db6b47c2d6079927fc05d4ec1b
corayma nuñez quispe <coraymanq@gmail.com>
2020-03-11 13:48:53 -0500

    subiendo el Header

--- moduloB/header.php
+++ moduloB/header.php
@@ -1,20 +1,69 @@
-<html lang="es-ES">
-
+<!doctype html>
+<html class="no-js" lang="es-ES">
 <head>
-  <title>catalogo</title>
-   <script src="js/cargarUrl.js"></script>
-   <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+  <meta charset="utf-8">
+  <title>Soft. Catalogo -  Yakeline</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" type="text/css" href="css/template.css">
+  <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+  <link href="https://fonts.googleapis.com/css?family=Barlow&display=swap" rel="stylesheet"> 
+
+  <meta name="theme-color" content="#fafafa">
 </head>
 
 <body>
-  <div class="wrapper ">
-  	<h4>Menu</h4>
-  	<ul>
-	    <li class="nav-item ">
-	    	<a href="caja.php">Caja</a>
-	    </li>
-	    <li>
-	    	<a href="catalogo.php">Catalogo</a>
-	    </li>
-  	</ul>
-   
+  <!--[if IE]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+  <!-- Add your site or application content here -->
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <link rel="icon" href="../../../../favicon.ico">
+  
+      <title>Top navbar example for Bootstrap</title>
+  
+      <!-- Bootstrap core CSS -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  
+      <!-- Custom styles for this template -->
+      <link href="navbar-top.css" rel="stylesheet">
+    </head>
+  
+    <body>
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+        <a class="navbar-brand" href="#">Top navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+          </ul>
+          <form class="form-inline mt-2 mt-md-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
