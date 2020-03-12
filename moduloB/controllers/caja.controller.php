671e6ac0b2b12c212d87bf914df462462a365bef
corayma nuñez quispe <coraymanq@gmail.com>
2020-03-12 14:06:23 -0500

    subiendo cambios al proyecto

--- moduloB/controllers/caja.controller.php
+++ moduloB/controllers/caja.controller.php
@@ -1,25 +1,56 @@
 <?php
+require("../models/Conexion.php");
 require("../models/caja.model.php");
 
-$idpersonal   = $_POST['idpersonal'];
-$caja         = $_POST['caja'];
-$fondo        = $_POST['fondo'];
-$seccion      = $_POST['seccion'];
-$fechaextrema = $_POST['fechaextrema'];
-$fecCreated   = $_POST['fecCreated'];
-$estado       = $_POST['estado'];
-$fecCierre    = $_POST['fecCierre'];
+$link = new Conexion();
+$conn = $link->Conectar();
+
+//if(!empty($_POST['idpersonal']) && !empty($_POST['caja']) && $_POST['fondo']) && !empty($_POST['seccion']) && $_POST['fechaextremainicio']) && !empty($_POST['fechaextremafinal']) && $_POST['expedienteinicio']) && !empty($_POST['expedientefinal']))
+if(!empty($_POST['idpersonal']) && !empty($_POST['caja']) && !empty($_POST['fondo']) && !empty($_POST['seccion']) && !empty($_POST['fechaextremainicio']) && !empty($_POST['fechaextremafinal']) && !empty($_POST['expedienteinicio']) && !empty($_POST['expedientefinal']))
+{
+
+	$idpersonal         = $_POST['idpersonal'];
+	$caja               = $_POST['caja'];
+	$fondo              = $_POST['fondo'];
+	$seccion            = $_POST['seccion'];
+	$fechaextremainicio = $_POST['fechaextremainicio'];
+	$fechaextremafinal  = $_POST['fechaextremafinal'];
+	$expedienteinicio   = $_POST['expedienteinicio'];
+	$expedientefinal    = $_POST['expedientefinal'];
+
+	$caja2 = "Caja ".$caja;
+
+	echo "idpersonal: "$idpersonal."<br>";
+
+	$sql = "SELECT * FROM caja WHERE estado = 1 AND idpersonal = ". $idpersonal;
+
+	$res = $conn->query($sql);
+	$cantidad_cajas = $res->num_rows;
+	
+	//echo "Numero: ". $cantidad_cajas;
+
+	if($cantidad_cajas > 2){
+		echo "Solo esta permitido trabajar con 2 proyectos al mismo tiempo.  Por favor culmine la Descripcion de alguna Caja que ya esta trabajando, y cuando termine cierre el proyecto, y posteriormente puede crear otro proyecto nuevo. Le agradecemos su comprensión";
+
+	}else{
+		
+		$cajas = new Caja();
+		$cajas->Guardar($idpersonal,$caja2,$fondo,$seccion,$fechaextremainicio,$fechaextremafinal,$expedienteinicio,$expedientefinal);
+		
+		echo "Registrado Exitosamente";
+	}
+
+}else{
+	echo "Valio";
+}
 
 
 
-
-
-$cajas = new Caja();
-$cajas->Guardar($idpersonal, $caja, $fondo, $seccion, $fechaextrema, $fecCreated, $estado, $fecCierre);
-
+/*
 header("Location: ../index.php");
 ?>
 
 <script type="text/javascript">
   window.close();
 </script>
+*/
